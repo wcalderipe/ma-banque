1411e4cff79db7ee7aca4e4a645995e87df70c61
William Calderipe <wcalderipe@gmail.com>
2020-06-23 19:11:50 +0200

    Remove Active Storage, Mailbox, Text and View
    Keeping the application as clean as possible.

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
 require "action_controller/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

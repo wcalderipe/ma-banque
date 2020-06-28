2f9cc86385521d11911fa256f2eec2ef0d29b3b9
William Calderipe <wcalderipe@gmail.com>
2020-06-28 11:32:47 +0200

    Refactor account create command spec
    This commits adds an extra spec to the account create command to show
    the cascade of events created after the `call` method is invoked. It
    intentionally doesn't assert the values of the update status event
    neither the aggregate and leave this responsibility to the reactor
    unit test.

--- spec/models/commands/account/create_spec.rb
+++ spec/models/commands/account/create_spec.rb
@@ -1,9 +1,29 @@
 require "rails_helper"
 
 describe Commands::Account::Create do
-  it "creates an account" do
+  subject { described_class.call(metadata: { source: "test" }) }
+
+  it "creates an event with metadata" do
     expect {
-      Commands::Account::Create.call(metadata: { source: "test" })
-    }.to change { Account.count }.by(1)
+      subject
+    }.to change { Events::Account::Created.count }.by(1)
+
+    last_account_created = Events::Account::Created.last
+
+    expect(last_account_created.metadata).to eq({ "source" => "test" })
+  end
+
+  it "creates an account" do
+    expect { subject }.to change { Account.count }.by(1)
+  end
+
+  it "triggers account validator reactor after create" do
+    expect {
+      subject
+    }.to change { Events::Account::BaseEvent.count }.by(2)
+
+    last_event = Events::Account::BaseEvent.last
+
+    expect(last_event).to be_a(Events::Account::StatusUpdated)
   end
 end
--- spec/models/events/account/base_event_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-describe Events::Account::BaseEvent do
-  describe ".table_name" do
-    it do
-      expect(Events::Account::BaseEvent.table_name).to eq("account_events")
-    end
-  end
-end

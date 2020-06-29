b7db234dc43ea1cf916fe062f9ce112216def348
William Calderipe <wcalderipe@gmail.com>
2020-06-29 08:18:01 +0200

    Use online module definition

--- app/lib/event_source/command.rb
+++ app/lib/event_source/command.rb
@@ -32,81 +32,79 @@
 #     end
 #   end
 # ```
-module EventSource
-  module Command
-    extend ActiveSupport::Concern
+module EventSource::Command
+  extend ActiveSupport::Concern
 
-    included do
-      include ActiveModel::Validations
+  included do
+    include ActiveModel::Validations
+  end
+
+  class_methods do
+    # Run validations and persist the event.
+    #
+    # On success: returns the event
+    # On noop: returns nil
+    # On failure: raise an ActiveRecord::RecordInvalid error
+    def call(*args)
+      new(*args).call
     end
 
-    class_methods do
-      # Run validations and persist the event.
-      #
-      # On success: returns the event
-      # On noop: returns nil
-      # On failure: raise an ActiveRecord::RecordInvalid error
-      def call(*args)
-        new(*args).call
-      end
+    # Define the attributes.
+    #
+    # They are set when initializing the command as keyword
+    # arguments and are all accessible as getter methods.
+    #
+    # Example: `attributes :post, :user, :ability`
+    def attributes(*args)
+      attr_reader(*args)
 
-      # Define the attributes.
-      #
-      # They are set when initializing the command as keyword
-      # arguments and are all accessible as getter methods.
-      #
-      # Example: `attributes :post, :user, :ability`
-      def attributes(*args)
-        attr_reader(*args)
+      initialize_method_arguments = args.map { |arg| "#{arg}:" }.join(', ')
+      initialize_method_body = args.map { |arg| "@#{arg} = #{arg}" }.join(";")
 
-        initialize_method_arguments = args.map { |arg| "#{arg}:" }.join(', ')
-        initialize_method_body = args.map { |arg| "@#{arg} = #{arg}" }.join(";")
-
-        class_eval <<~CODE
+      class_eval <<~CODE
           def initialize(#{initialize_method_arguments})
             #{initialize_method_body}
             after_initialize
           end
         CODE
-      end
     end
+  end
 
-    def call
-      return nil if event.nil?
+  def call
+    return nil if event.nil?
 
-      raise "The event should not be persisted at this stage!" if event.persisted?
+    raise "The event should not be persisted at this stage!" if event.persisted?
 
-      validate!
+    validate!
 
-      execute!
+    execute!
 
-      event
-    end
+    event
+  end
 
-    def validate!
-      valid? || raise(ActiveRecord::RecordInvalid, self)
-    end
+  def validate!
+    valid? || raise(ActiveRecord::RecordInvalid, self)
+  end
 
-    # A new record or nil if noop
-    def event
-      @event ||= build_event
-    end
+  # A new record or nil if noop
+  def event
+    @event ||= build_event
+  end
 
-    # Save the event. Should not be overwritten by the command as side
-    # effects should be implemented via Reactors triggering other
-    # Events.
-    private def execute!
-      event.save!
-    end
+  # Save the event. Should not be overwritten by the command as side
+  # effects should be implemented via Reactors triggering other
+  # Events.
+  private def execute!
+    event.save!
+  end
 
-    # Returns a new event record or nil if noop.
-    private def build_event
-      raise NotImplementedError
-    end
+  # Returns a new event record or nil if noop.
+  private def build_event
+    raise NotImplementedError
+  end
 
-    # Hook to set default values.
-    private def after_initialize
-      # noop
-    end
+  # Hook to set default values.
+  private def after_initialize
+    # noop
   end
 end

3e613b5469943bae0235ae11e855b77354098bbf
William Calderipe <wcalderipe@gmail.com>
2020-07-05 16:25:52 +0200

    Set account balance to zero on create

--- app/domains/events/banking/account/created.rb
+++ app/domains/events/banking/account/created.rb
@@ -1,6 +1,7 @@
 class Events::Banking::Account::Created < Events::Banking::Account::BaseEvent
   def apply(account)
     account.status = Banking::Account::PENDING
+    account.balance = 0
 
     account
   end
--- spec/domain/events/banking/account/created_spec.rb
+++ spec/domain/events/banking/account/created_spec.rb
@@ -2,12 +2,20 @@ require "rails_helper"
 
 describe Events::Banking::Account::Created do
   describe "#apply" do
-    it "sets account status to pending" do
+    it "sets status to pending" do
       account = Banking::Account.new(status: nil)
 
       subject.apply(account)
 
       expect(account.status).to eq(Banking::Account::PENDING)
+    end
+
+    it "sets balance to zero" do
+      account = Banking::Account.new(balance: nil)
+
+      subject.apply(account)
+
+      expect(account.balance).to eq(0)
     end
   end
 end
